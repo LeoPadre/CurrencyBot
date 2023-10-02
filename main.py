bcd694f01fe1c0f86a25a45cee8778b432564c56
Levon Osipov <levon.osipov@yahoo.com>
2023-10-02 19:42:17 +0300

    правка комментариев в коде

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ import requests
 import xml.etree.ElementTree as ET
 import datetime
 
-# Загрузить XML-содержимое по ссылке
+# Загрузка XML-содержимого по ссылке
 current_date = datetime.date.today()
 current_date_str = current_date.strftime("%d.%m.%Y")
 url = "https://www.cbr.ru/scripts/XML_daily.asp?date_req=" + current_date_str.replace('.', '/')
 
 response = requests.get(url)
 
-# Проверить, что запрос прошел успешно
+# Проверка успешного выполнения запроса
 if response.status_code == 200:
-    # Разобрать XML-содержимое
+    # Разбор XML-содержимого
     xml_content = response.content
     root = ET.fromstring(xml_content)
 
-    # Создать пустой список для данных
+    # Создание пустого списка для данных
     data = []
 
-    # Пройти по элементам XML и извлечь нужные данные
+    # Проход по элементам XML и извлечение нужных данных
     for valute in root.findall('Valute'):
         name = valute.find('Name').text
         vunit_rate = valute.find('VunitRate').text
-        # Преобразовать строку в число с двумя знаками после запятой в меньшую сторону
+        # Преобразование строки в число с двумя знаками после запятой в меньшую сторону
         vunit_rate = round(float(vunit_rate.replace(',', '.')), 2)
         data.append((name, vunit_rate))
 
